78dce90e109f196057a5bba74da8849d2301a915
Mattheus Faria <mattheusfaria27@gmail.com>
2023-10-26 23:49:26 -0400

    Added list search and title grouping

--- IRS 990 Scrapper.py
+++ IRS 990 Scrapper.py
@@ -11,6 +11,7 @@ locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
 # To-Do List
 # Finish Commenting Functions
 # Fix Excel Corruption
+# For some reason, title group "President" cannot be read
 # Better Way of searching for colleges
 #   Probably going to be some form of web creation at this point
 
@@ -27,8 +28,8 @@ def get_irs_990_web_content(search_result):
 
     # Use BeautifulSoup library to find the first xml button on the website and get the link to the xml report
     page_soup = BeautifulSoup(page, "html.parser")
-    xml_soup = page_soup.find(class_="action xml")
-    xml_url_query = xml_soup.get("href")
+    xml_soup = page_soup.find_all(lambda tag: tag.name == "a" and "XML" in tag.text)
+    xml_url_query = xml_soup[0].get("href")
     xml_url = "https://projects.propublica.org" + xml_url_query
     xml_search_result = requests.get(xml_url).content
 
@@ -37,7 +38,7 @@ def get_irs_990_web_content(search_result):
     return irs_990_soup
 
 def get_institution_occupation_data(irs_990_soup):
-    columns = ["Name", "Title", "Base Compensation", "Other Comp", "Total Comp"]
+    columns = ["Name", "Title", "Title Group", "Base Compensation", "Other Comp", "Total Comp"]
     jobs = pd.DataFrame(columns=columns)
     company_wide_compensation = 0
     tax_filing_soup = irs_990_soup.find("IRS990")
@@ -58,9 +59,13 @@ def get_institution_occupation_data(irs_990_soup):
         if total_comp == 0:
             continue
 
+        title_name = employee_soup.find("TitleTxt").text.strip()
+        title_group = get_title_group(title_name)
+
         job_info = {
             "Name" : name,
-            "Title": employee_soup.find("TitleTxt").text.strip(),
+            "Title": title_name,
+            "Title Group" : title_group,
             "Base Compensation" : base_comp,
             "Other Comp" : other_comp,
             "Total Comp" : total_comp
@@ -69,6 +74,31 @@ def get_institution_occupation_data(irs_990_soup):
 
     top_jobs = jobs.sort_values(by=['Total Comp'], ascending=False)
     return top_jobs, company_wide_compensation
+
+def get_title_group(title_name):
+    tg = "Other"
+    t = title_name.lower()
+    list_of_titles = [
+        "Vice President",
+        "Vice Provost",
+        "President", 
+        "Provost", 
+        "VP", 
+        "Trustee",
+        "Dean", 
+        "Exec", 
+        "Prof",
+        "Treas",
+        "Secretary",
+        "Chief",
+        "Dept Head"
+    ]
+    
+    for title in list_of_titles:
+        if title.lower() in t:
+            tg = title
+            break
+    return tg
 
 def get_institution_revenue_data(irs_990_soup):
     filer = irs_990_soup.find("Filer")
@@ -101,7 +131,7 @@ def institute_summary(revenue_dict, top_jobs):
     print(f"Total Company Wide Compensation     :", locale.currency(revenue_dict["company_wide_compensation"], grouping=True))
     print(top_jobs[0:10])
 
-def write_intitution_to_excel(revenue_dict, top_jobs):
+def write_intitution_to_excel(revenue_dict, top_jobs, nonprofit_subtitle):
     excelWorkbook = load_workbook("Master.xlsx")
     writer = pd.ExcelWriter("Master.xlsx", engine='openpyxl')
     writer.book = excelWorkbook
@@ -123,9 +153,10 @@ def write_intitution_to_excel(revenue_dict, top_jobs):
 
     sheet["B7"] = "Name"
     sheet["C7"] = "Title"
-    sheet["D7"] = "Base Compensation"
-    sheet["E7"] = "Other Comp"
-    sheet["F7"] = "Total Comp"
+    sheet["D7"] = "Title Group"
+    sheet["E7"] = "Base Compensation"
+    sheet["F7"] = "Other Comp"
+    sheet["G7"] = "Total Comp"
 
     row = 8
     index = 1
@@ -134,48 +165,69 @@ def write_intitution_to_excel(revenue_dict, top_jobs):
         sheet["A"+str(row)] = index
         sheet["B"+str(row)] = job["Name"]
         sheet["C"+str(row)] = job["Title"]
-        sheet["D"+str(row)] = job["Base Compensation"]
-        sheet["E"+str(row)] = job["Other Comp"]
-        sheet["F"+str(row)] = job["Total Comp"]
+        sheet["D"+str(row)] = job["Title Group"]
+        sheet["E"+str(row)] = job["Base Compensation"]
+        sheet["F"+str(row)] = job["Other Comp"]
+        sheet["G"+str(row)] = job["Total Comp"]
         row += 1
         index += 1
 
     writer.save()
     writer.close()
 
-# Ask for Institution Name + Excel Sheet Name
-nonprofit_search_query = input("Full Institute Name: ")
-nonprofit_subtitle = input("Institute Nickname: ")
-# If no subtitle is provided, just make it the search query
-if nonprofit_subtitle == "":
-    nonprofit_subtitle = nonprofit_search_query
+def search(nonprofit_search_query, nonprofit_subtitle, show_summary):
+    # Use ProPublica API to get basic institution data
+    search_url = "https://projects.propublica.org/nonprofits/api/v2/search.json?"
+    parameters = urllib.parse.urlencode({
+        'q' : nonprofit_search_query
+    })
+    search_result = requests.get(search_url+parameters)
 
-# Use ProPublica API to get basic institution data
-search_url = "https://projects.propublica.org/nonprofits/api/v2/search.json?"
-parameters = urllib.parse.urlencode({
-      'q' : nonprofit_search_query
-  })
-search_result = requests.get(search_url+parameters)
+    # If an institution is found, continue; break if nothing is found
+    if search_result.status_code == 200:
 
-# If an institution is found, continue; break if nothing is found
-if search_result.status_code == 200:
+        # Get the web content in the form of an xml version of an IRS 990 form
+        irs_990_soup = get_irs_990_web_content(search_result)
 
-    # Get the web content in the form of an xml version of an IRS 990 form
-    irs_990_soup = get_irs_990_web_content(search_result)
+        # Use xml content to get the paid reported occupations sorted by highest compensation descending
+        top_jobs, company_wide_compensation = get_institution_occupation_data(irs_990_soup)
 
-    # Use xml content to get the paid reported occupations sorted by highest compensation descending
-    top_jobs, company_wide_compensation = get_institution_occupation_data(irs_990_soup)
+        # Get the basic IRS data from the xml content
+        revenue_dict = get_institution_revenue_data(irs_990_soup)
+        revenue_dict["company_wide_compensation"] = company_wide_compensation
 
-    # Get the basic IRS data from the xml content
-    revenue_dict = get_institution_revenue_data(irs_990_soup)
-    revenue_dict["company_wide_compensation"] = company_wide_compensation
+        # Erase Comment to get a terminal summary of the information found
+        if(show_summary):
+            institute_summary(revenue_dict, top_jobs)
 
-    # Erase Comment to get a terminal summary of the information found
-    institute_summary(revenue_dict, top_jobs)
-
-    # Write all the information to the Master Excel file
-    write_intitution_to_excel(revenue_dict, top_jobs)
+        # Write all the information to the Master Excel file
+        write_intitution_to_excel(revenue_dict, top_jobs, nonprofit_subtitle)
     
+    else:
+        print("Could not find", nonprofit_search_query)
+
+
+print("If reading from list, enter 'list'. If not, just hit enter.")
+using_list = input("Using list?: ")
+if using_list == "list":
+    print("Reading from file...")
+    institutes = []
+    with open("Institutes.txt", "r") as f:
+        institutes = f.readlines()
+    for i in range(len(institutes)):
+        institutes[i] = institutes[i][:-1]
+    for i in range(len(institutes)):
+        if i % 2 == 0:
+            search(institutes[i], institutes[i+1], False)
+        else:
+            continue
+
 else:
-    print("Could not find", nonprofit_search_query)
+    # Ask for Institution Name + Excel Sheet Name
+    nonprofit_search_query = input("Full Institute Name: ")
+    nonprofit_subtitle = input("Institute Nickname: ")
+    # If no subtitle is provided, just make it the search query
+    if nonprofit_subtitle == "":
+        nonprofit_subtitle = nonprofit_search_query
+    search(nonprofit_search_query, nonprofit_subtitle, True)
 
